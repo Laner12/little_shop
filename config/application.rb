1e7fa7e97ec78743fbd89a89103b613219a40fc1
Angela Lindow <angelalindow@gmail.com>
2016-07-28 15:36:10 -0600

    assets

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,6 @@ Bundler.require(*Rails.groups)
 
 module LittleShop
   class Application < Rails::Application
-    config.assets.precompile += [
-      'bootstrap_and_overrides.css',
-      'flash_styling.css',
-      'navbar_styling.css'
-    ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
