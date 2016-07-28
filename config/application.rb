ef3c89289ab7d10107b2c386da4860ff7b80f11d
Angela Lindow <angelalindow@gmail.com>
2016-07-28 15:30:03 -0600

    assets

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.precompile += [
-'bootstrap_and_overrides.css',
-'flash_styling.css',
-'navbar_styling.css'
-]
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,6 +9,11 @@ Bundler.require(*Rails.groups)
 
 module LittleShop
   class Application < Rails::Application
+    config.assets.precompile += [
+      'bootstrap_and_overrides.css',
+      'flash_styling.css',
+      'navbar_styling.css'
+    ]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
