6f531de902c3082001901726b822c3ddc29cb46a
Lane Winham <mlwinham12@gmail.com>
2016-08-01 20:47:58 -0600

    Fix bug for oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
-      new_user.uid             = auth_info.extra.raw_info.uid
+      new_user.uid             = auth_info.uid
       new_user.username        = auth_info.extra.raw_info.screen_name
       new_user.oauth_token     = auth_info.credentials.token
       new_user.password_digest = auth_info.credentials.secret
